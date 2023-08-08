016cd1d839af2df6f2e8782ca5d86149ecb03531
acabr017 <arielcabrera3@gmail.com>
2023-08-08 14:54:14 -0400

    Making my own branch for testing/changing things

--- excel_converter.py
+++ excel_converter.py
@@ -2,23 +2,30 @@ import pandas as pd
 import os
 import csv
 
-files = ["A Average Citizenship Honor Roll","Citizenship Honor Roll","Principal Honor Roll","Regular Honor Roll","Superior Honor Roll", "totals"]
+files = [
+    "A Average Citizenship Honor Roll",
+    "Citizenship Honor Roll",
+    "Principal Honor Roll",
+    "Regular Honor Roll",
+    "Superior Honor Roll",
+    "totals",
+]
 
 
-def set_save_directory(save_dir:str):
+def set_save_directory(save_dir: str):
     global save_directory
-    save_directory = save_dir.replace('/','\\')
+    save_directory = save_dir.replace("/", "\\")
 
 
-def set_filename(file_name:str):
+def set_filename(file_name: str):
     global filename
     filename = file_name
 
 
-def file_clean_up(file_type:str):
+def file_clean_up(file_type: str):
     for item in files:
-        item = item.replace(" ","").lower() + "_" + filename
-        path = os.path.join(save_directory,item) + "." + file_type
+        item = item.replace(" ", "").lower() + "_" + filename
+        path = os.path.join(save_directory, item) + "." + file_type
         print(f"Using path.join: {path}")
         print(os.path.exists(path))
         if os.path.exists(path):
@@ -26,21 +33,21 @@ def file_clean_up(file_type:str):
     print("Cleaned up!")
 
 
-def final_file_clean_up(file_type:str):
+def final_file_clean_up(file_type: str):
     to_delete = "_" + filename + "." + file_type
-    path = os.path.join(save_directory,to_delete)
+    path = os.path.join(save_directory, to_delete)
     if os.path.exists(path):
-            os.remove(path)
+        os.remove(path)
 
 
-def Excel_to_text(sheet_as_string:str):
+def Excel_to_text(sheet_as_string: str):
     currentRoll = ""
     for index, row in enumerate(sheet_as_string):
         if index == 0:
             continue
         row = row[2:]
         if "roll" in row.lower():
-            rollType = row[len(row)-32::1].strip().replace(" ","").lower()
+            rollType = row[len(row) - 32 :: 1].strip().replace(" ", "").lower()
             currentRoll = rollType
             continue
         if ("STU ID" in row) or ("NaN" in row):
@@ -48,17 +55,17 @@ def Excel_to_text(sheet_as_string:str):
         items = list(filter(lambda x: len(x) > 2, row.split("               ")))
         for val in items:
             with open(f"{save_directory+'/'+currentRoll+'_'+filename}.txt", "a") as f:
-                print(val.strip(),file=f)
+                print(val.strip(), file=f)
 
 
-def Excel_to_csv(sheet_as_string:str):
+def Excel_to_csv(sheet_as_string: str):
     currentRoll = ""
     for index, row in enumerate(sheet_as_string):
         if index == 0:
             continue
         row = row[2:]
         if "roll" in row.lower():
-            rollType = row[len(row)-32::1].strip().replace(" ","").lower()
+            rollType = row[len(row) - 32 :: 1].strip().replace(" ", "").lower()
             currentRoll = rollType
             continue
         if ("STU ID" in row) or ("NaN" in row):
@@ -66,47 +73,48 @@ def Excel_to_csv(sheet_as_string:str):
         items = list(filter(lambda x: len(x) > 2, row.split("               ")))
         with open(f"{save_directory+'/'+currentRoll+'_'+filename}.csv", "a") as f:
             writer = csv.writer(f)
-            with open(f"{save_directory+'/'+currentRoll+'_'+filename}.csv", "r") as csvfile:
+            with open(
+                f"{save_directory+'/'+currentRoll+'_'+filename}.csv", "r"
+            ) as csvfile:
                 if sum(1 for line in csvfile) == 0:
-                    header = ['STU ID',' GR-HR', 'STUDENT NAME']
+                    header = ["STU ID", " GR-HR", "STUDENT NAME"]
                     writer.writerow(header)
             for line in items:
-                line = ' '.join(line.split("  ")).split()
+                line = " ".join(line.split("  ")).split()
                 writer.writerow(line)
 
 
 def Line_counter():
     with open(f"{save_directory}/totals_{filename}.txt", "a") as f1:
         for item in files:
-            fileName = item.replace(" ","").lower() + '_' + filename
+            fileName = item.replace(" ", "").lower() + "_" + filename
             if fileName == "totals":
                 continue
             try:
-                with open(f"{fileName}.txt", 'r') as f2:
+                with open(f"{fileName}.txt", "r") as f2:
                     amountOfLines = len(f2.readlines())
                     print(f"{item}: {amountOfLines}", file=f1)
             except FileNotFoundError:
                 print(f"{item}: 0", file=f1)
-        
 
-def create_dataframes(filepath:str):
+
+def create_dataframes(filepath: str):
     return pd.read_excel(filepath, skiprows=7, header=None, sheet_name=None)
 
 
-def convert_dataframe(dataframe,output_type:str):
+def convert_dataframe(dataframe, output_type: str):
     for key, values in dataframe.items():
         if key == "Sheet1" or key == f"Sheet{len(dataframe.values())}":
             continue
         excel_string = values.to_string().split("\n")
-        if output_type.lower() == 'txt':
+        if output_type.lower() == "txt":
             Excel_to_text(excel_string)
-        elif output_type.lower() == 'csv':
+        elif output_type.lower() == "csv":
             Excel_to_csv(excel_string)
 
 
-
-
 # test things
+
 
 def testing_globals():
     print(f"Global Filename: {filename} and Global Directory: {save_directory}")
@@ -114,6 +122,6 @@ def testing_globals():
 
 def testing_file_creation():
     for item in files:
-        item = item.replace(" ","").lower() + " " + filename
-        with open(item,"w") as f:
+        item = item.replace(" ", "").lower() + " " + filename
+        with open(item, "w") as f:
             print("Testing", file=f)
--- excelappgui.py
+++ excelappgui.py
@@ -16,24 +16,25 @@ get_files_path = "C://"
 
 def start_button_state():
     if len(file_name) > 0 and output_locatin_selected and output_type_selected:
-        start_button['state'] = tk.NORMAL
-        start_button['fg'] = 'green'
+        start_button["state"] = tk.NORMAL
+        start_button["fg"] = "green"
     else:
-        start_button['state'] = tk.DISABLED
-        start_button['bg'] = 'white'
+        start_button["state"] = tk.DISABLED
+        start_button["bg"] = "white"
 
 
 def get_files_func():
     global file_name
-    file_name = list(filedialog.askopenfilenames(initialdir=get_files_path,
-                                                 title="Select File(s)"))
+    file_name = list(
+        filedialog.askopenfilenames(initialdir=get_files_path, title="Select File(s)")
+    )
     for file in file_name:
         if file.endswith(".xls") or file.endswith(".xlsx"):
             insert_to_input_table(clean_string_for_table_insert(file), file)
         else:
-            messagebox.showerror("Warning!",
-                                 "Only .xls and .xlsx files accepted!",
-                                 parent=window)
+            messagebox.showerror(
+                "Warning!", "Only .xls and .xlsx files accepted!", parent=window
+            )
     start_button_state()
 
 
@@ -52,17 +53,17 @@ def insert_to_output_table(f_name: str, f_path: str):
 def toggle_delete(_):
     print(f"Event Triggered - Condition is {len(file_name) > 0}")
     if len(input_table.selection()) > 0:
-        file_delete_button['state'] = tk.NORMAL
-        file_delete_button['fg'] = 'red'
+        file_delete_button["state"] = tk.NORMAL
+        file_delete_button["fg"] = "red"
     else:
-        file_delete_button['state'] = tk.DISABLED
-        file_delete_button['bg'] = 'white'
+        file_delete_button["state"] = tk.DISABLED
+        file_delete_button["bg"] = "white"
     start_button_state()
 
 
 def delete_items(_):
     for i in input_table.selection():
-        file_name.remove(input_table.item(i)['values'][1])
+        file_name.remove(input_table.item(i)["values"][1])
         input_table.delete(i)
 
 
@@ -77,7 +78,7 @@ def set_directory_func():
 
 
 def get_filename(file_path_string: str):
-    return file_path_string.split("/")[-1].replace(" ", "").split('.')[0]
+    return file_path_string.split("/")[-1].replace(" ", "").split(".")[0]
 
 
 def make_new_dir(dir_path: str):
@@ -85,17 +86,21 @@ def make_new_dir(dir_path: str):
 
 
 def get_output_files():
-    return [fn for fn in os.listdir(output_location) if
-            fn.lower().endswith(output_type) and
-            os.path.isfile(os.path.join(output_location, fn))]
+    return [
+        fn
+        for fn in os.listdir(output_location)
+        if fn.lower().endswith(output_type)
+        and os.path.isfile(os.path.join(output_location, fn))
+    ]
 
 
 def populate_output_table():
     output_files = get_output_files()
     print(output_files)
     for file in output_files:
-        insert_to_output_table(clean_string_for_table_insert(file),
-                               f"{output_location}/{file}")
+        insert_to_output_table(
+            clean_string_for_table_insert(file), f"{output_location}/{file}"
+        )
 
 
 def start_button_func():
@@ -120,6 +125,7 @@ def radio_button_func():
     output_type = output_type_string.get()
     start_button_state()
 
+
 # -------------- Window --------------
 
 
@@ -139,51 +145,40 @@ window_height = int(window.winfo_screenmmheight())
 
 
 # Instructions Frame
-instructions_frame = tk.Frame(window,
-                              borderwidth=5,
-                              width=570,
-                              height=150,
-                              relief='groove')
+instructions_frame = tk.Frame(
+    window, borderwidth=5, width=570, height=150, relief="groove"
+)
 # instructions_frame.pack_propagate(False)
 
 # Setup Buttons Frame
-setup_buttons_frame = tk.Frame(window,
-                               borderwidth=5,
-                               width=int(window_width*1.2),
-                               height=100)
+setup_buttons_frame = tk.Frame(
+    window, borderwidth=5, width=int(window_width * 1.2), height=100
+)
 setup_buttons_frame.pack_propagate(False)
 
 # Output Files Label Frame
-output_files_label_frame = tk.Frame(window,
-                                    width=int(window_width*1.2),
-                                    height=50,
-                                    relief='groove')
+output_files_label_frame = tk.Frame(
+    window, width=int(window_width * 1.2), height=50, relief="groove"
+)
 setup_buttons_frame.pack_propagate(False)
 
 # Frame to hold table label Frame
-placeholder_frame = tk.Frame(window,
-                             width=int(window_width*1.2),
-                             height=50)
+placeholder_frame = tk.Frame(window, width=int(window_width * 1.2), height=50)
 placeholder_frame.pack_propagate(False)
 
 # Table Label Frame
-table_label_frame = tk.Frame(placeholder_frame,
-                             width=int(window_width*1.2),
-                             height=40,
-                             relief='groove')
+table_label_frame = tk.Frame(
+    placeholder_frame, width=int(window_width * 1.2), height=40, relief="groove"
+)
 
 table_label_frame.pack_propagate(False)
 
 # Frame for Radio Buttons
-radio_frame = ttk.Frame(setup_buttons_frame,
-                        width=200,
-                        height=80)
+radio_frame = ttk.Frame(setup_buttons_frame, width=200, height=80)
 radio_frame.pack_propagate(False)
 
 # Frame for Start and Delete Buttons
-del_and_start_buttons_frame = tk.Frame(window,
-                                       width=570,
-                                       height=50)
+del_and_start_buttons_frame = tk.Frame(window, width=570, height=50)
 del_and_start_buttons_frame.pack_propagate(False)
 
 # Input Table Frame
@@ -192,119 +187,127 @@ input_table_frame = tk.Frame(window)
 # :::::::::::::::::: Buttons ::::::::::::::::::
 
 # Get File Button
-get_files_button = tk.Button(setup_buttons_frame,
-                             text="Upload Files",
-                             command=get_files_func,
-                             font=('Times New Roman', 14))
+get_files_button = tk.Button(
+    setup_buttons_frame,
+    text="Upload Files",
+    command=get_files_func,
+    font=("Times New Roman", 14),
+)
 
 # File Delete Button
-file_delete_button = tk.Button(del_and_start_buttons_frame,
-                               text="                   \
+file_delete_button = tk.Button(
+    del_and_start_buttons_frame,
+    text="                   \
                                 Delete                   ",
-                               state="disabled",
-                               command=lambda: delete_items(""),
-                               font=('Times New Roman', 14))
+    state="disabled",
+    command=lambda: delete_items(""),
+    font=("Times New Roman", 14),
+)
 
 # Set Directory Button
-set_directory_button = tk.Button(setup_buttons_frame,
-                                 text="Set Directory For Output Files",
-                                 command=set_directory_func,
-                                 font=('Times New Roman', 14))
+set_directory_button = tk.Button(
+    setup_buttons_frame,
+    text="Set Directory For Output Files",
+    command=set_directory_func,
+    font=("Times New Roman", 14),
+)
 
 # Start Button
-start_button = tk.Button(del_and_start_buttons_frame,
-                         text="                    \
+start_button = tk.Button(
+    del_and_start_buttons_frame,
+    text="                    \
                             Start                    ",
-                         state='disabled',
-                         command=start_button_func,
-                         font=('Times New Roman', 14))
+    state="disabled",
+    command=start_button_func,
+    font=("Times New Roman", 14),
+)
 
 # Radio Buttons for Output Type:
 output_type_string = tk.StringVar()
 
 
-txt_radio_button = ttk.Radiobutton(radio_frame,
-                                   text="Text Output",
-                                   value="txt",
-                                   variable=output_type_string,
-                                   command=radio_button_func)
-csv_radio_button = ttk.Radiobutton(radio_frame,
-                                   text="CSV Output",
-                                   value='csv',
-                                   variable=output_type_string,
-                                   command=radio_button_func)
+txt_radio_button = ttk.Radiobutton(
+    radio_frame,
+    text="Text Output",
+    value="txt",
+    variable=output_type_string,
+    command=radio_button_func,
+)
+csv_radio_button = ttk.Radiobutton(
+    radio_frame,
+    text="CSV Output",
+    value="csv",
+    variable=output_type_string,
+    command=radio_button_func,
+)
 
 
 # :::::::::::::::::: Treeview :::::::::::::::::
 # Input and Output Tables
 
 style = ttk.Style()
-style.configure('TRadiobutton',
-                font='timesnewroman 12',
-                foreground='black')
+style.configure("TRadiobutton", font="timesnewroman 12", foreground="black")
 
-input_table = ttk.Treeview(input_table_frame,
-                           columns=(1, 2),
-                           show='headings')
+input_table = ttk.Treeview(input_table_frame, columns=(1, 2), show="headings")
 
-input_table.heading(1,
-                    text="Input File Name")
-input_table.heading(2,
-                    text="Input File Path")
-input_table.column(1,
-                   width=15)
+input_table.heading(1, text="Input File Name")
+input_table.heading(2, text="Input File Path")
+input_table.column(1, width=15)
 
 
-output_table = ttk.Treeview(window,
-                            columns=('file', 'filepath'),
-                            show='headings')
-output_table.heading('file',
-                     text="Output File Name")
-output_table.heading('filepath',
-                     text="Output File Path")
-output_table.column('file',
-                    width=15)
+output_table = ttk.Treeview(window, columns=("file", "filepath"), show="headings")
+output_table.heading("file", text="Output File Name")
+output_table.heading("filepath", text="Output File Path")
+output_table.column("file", width=15)
 
 
 # :::::::::::::::::: Labels ::::::::::::::::::
 
 # Label for Directory
 directory_label_string = tk.StringVar()
-output_labels_frame_label = tk.Label(output_files_label_frame,
-                                     text="Files will be saved to:")
-directory_label = tk.Label(output_files_label_frame,
-                           textvariable=directory_label_string)
+output_labels_frame_label = tk.Label(
+    output_files_label_frame, text="Files will be saved to:"
+)
+directory_label = tk.Label(
+    output_files_label_frame, textvariable=directory_label_string
+)
 
 # Instructions Label
-instructions_text1 = '1. Upload the files to be converted.\n\
+instructions_text1 = "1. Upload the files to be converted.\n\
 2. Specify the output location.\n\
-3. Specify output file type (txt/csv)'
+3. Specify output file type (txt/csv)"
 
 instructions_text2 = 'Files will be generated in specified output location, \
 labled as "(rolltype)_(filename).(outputtype)"\n \
 Example: Uploaoding file: 5052 Q1.xls -> "regularhonorroll_5052Q1.txt"'
 
 
-instructions_label1 = tk.Label(instructions_frame,
-                               justify='left',
-                               text=instructions_text1,
-                               font=("Times New Roman", 14))
-instructions_label2 = tk.Label(instructions_frame,
-                               justify='center',
-                               text=instructions_text2,
-                               font=('Times New Roman', 12))
+instructions_label1 = tk.Label(
+    instructions_frame,
+    justify="left",
+    text=instructions_text1,
+    font=("Times New Roman", 14),
+)
+instructions_label2 = tk.Label(
+    instructions_frame,
+    justify="center",
+    text=instructions_text2,
+    font=("Times New Roman", 12),
+)
 
 
 # Table Label
 
-table_label = tk.Label(table_label_frame,
-                       justify='left',
-                       text='Selected Files',
-                       font=('Times New Roman', 11))
+table_label = tk.Label(
+    table_label_frame,
+    justify="left",
+    text="Selected Files",
+    font=("Times New Roman", 11),
+)
 
 # :::::::::::::::::: Events ::::::::::::::::::
 # events
-input_table.bind('<<TreeviewSelect>>', toggle_delete)
+input_table.bind("<<TreeviewSelect>>", toggle_delete)
 # table.bind('<Delete>', delete_items)
 
 
@@ -312,37 +315,37 @@ input_table.bind('<<TreeviewSelect>>', toggle_delete)
 # 1st Frame - Instructions
 instructions_frame.pack()
 instructions_label1.pack()
-instructions_label2.pack(side='bottom')
+instructions_label2.pack(side="bottom")
 
 # 2nd Frame - Upload button, directory button, output type radio buttons
 setup_buttons_frame.pack()
-get_files_button.pack(side='left')
-set_directory_button.pack(side='left', padx=100)
-radio_frame.pack(pady=20, side='left')
+get_files_button.pack(side="left")
+set_directory_button.pack(side="left", padx=100)
+radio_frame.pack(pady=20, side="left")
 txt_radio_button.pack()
 csv_radio_button.pack()
 
 # 3rd Frame - Output location labels
 output_files_label_frame.pack()
-output_labels_frame_label.pack(side='left')
-directory_label.pack(side='right')
+output_labels_frame_label.pack(side="left")
+directory_label.pack(side="right")
 
 # 4th Frame - Table Label
 placeholder_frame.pack()
-table_label_frame.pack(side='left')
-table_label.pack(side='left')
+table_label_frame.pack(side="left")
+table_label.pack(side="left")
 
 # 5th Frame - Input Table
-input_table_frame.pack(fill='both')
+input_table_frame.pack(fill="both")
 input_table.pack(padx=20, fill="x")
 
 # 6th Frame
 del_and_start_buttons_frame.pack()
-start_button.pack(pady=10, side='left', expand=True)
-file_delete_button.pack(expand=True, side='left')
+start_button.pack(pady=10, side="left", expand=True)
+file_delete_button.pack(expand=True, side="left")
 
 # Output Table
-output_table.pack(padx=20, fill='x')
+output_table.pack(padx=20, fill="x")
 
 
 # -------------- Run --------------
